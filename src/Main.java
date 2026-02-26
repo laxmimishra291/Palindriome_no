42fd05b94ad16b63cf276b58c8071028cfe432a0
laxmimishra291 <laxmimishra29102005@gmail.com>
2026-02-26 15:15:35 +0530

    UC3:Hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,29 @@
-class UseCase2PalindromeCheckerApp {
+import java.util.Scanner;
+
+class UseCase3PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Hardcoded string
-        String word = "madam";
+        Scanner sc = new Scanner(System.in);
 
-        // Reverse the string
+        // Take input from user
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+
+        // Reverse the string using loop
         String reversed = "";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
-        // Check palindrome using if-else
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome.");
+        // Compare original and reversed string
+        if (original.equals(reversed)) {
+            System.out.println("The string is a Palindrome.");
         } else {
-            System.out.println(word + " is NOT a Palindrome.");
+            System.out.println("The string is NOT a Palindrome.");
         }
 
+        sc.close();
     }
 }
