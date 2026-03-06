cd0d873a090bb8e0da7ba219c8128d34d9be2320
laxmimishra291 <laxmimishra29102005@gmail.com>
2026-03-06 16:35:44 +0530

    UC9: Add welcome message module

--- src/Main.java
+++ src/Main.java
@@ -1,93 +1,28 @@
 import java.util.Scanner;
 
-class Node {
-    char data;
-    Node next;
-
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-class LinkedListPalindrome {
-
-    Node head;
-
-    void add(char data) {
-        Node newNode = new Node(data);
-
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-
-        Node temp = head;
-        while (temp.next != null) {
-            temp = temp.next;
-        }
-
-        temp.next = newNode;
-    }
-
-    Node reverse(Node node) {
-        Node prev = null;
-        Node current = node;
-        Node next;
-
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        return prev;
-    }
-
-    boolean isPalindrome() {
-
-        if (head == null || head.next == null)
-            return true;
-
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
-    }
-}
-
 public class Main {
+
+    public static boolean isPalindrome(String str, int start, int end) {
+
+        if (start >= end) {
+            return true;
+        }
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        return isPalindrome(str, start + 1, end - 1);
+    }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        LinkedListPalindrome list = new LinkedListPalindrome();
 
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        for (char c : input.toCharArray()) {
-            list.add(c);
-        }
-
-        if (list.isPalindrome()) {
+        if (isPalindrome(input, 0, input.length() - 1)) {
             System.out.println("The given string is a Palindrome.");
         } else {
             System.out.println("The given string is NOT a Palindrome.");
